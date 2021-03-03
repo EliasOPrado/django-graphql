5ecd7f93328b882025a73a901ea943f6614d5c14
Elias Prado <eliaspradoprofessional@outlook.com>
2021-03-03 17:52:57 -0300

    added schema

--- gqlapp/models.py
+++ gqlapp/models.py
@@ -5,8 +5,8 @@ class ProductModel(models.Model):
     Segment = models.CharField(max_length=100)
     Country = models.CharField(max_length=100)
     Product = models.CharField(max_length=100)
-    Units = models.IntegerField(max_length=100)
-    Sales = models.IntegerField(max_length=100)
+    Units = models.IntegerField()
+    Sales = models.IntegerField()
     DateSold = models.CharField(max_length=100)
 
     def __str__(self):
